f4e7a8caeb668c5f50a17a600d1df5d7e744f6a0
Andrew Lin <landrew051127@gmail.com>
2022-11-08 21:23:47 +0800

    rgb update

--- firmware/crimson/rules.mk
+++ firmware/crimson/rules.mk
@@ -57,3 +57,5 @@ NKRO_ENABLE ?= yes		# USB Nkey Rollover - if this doesn't work, see here: https:
 BACKLIGHT_ENABLE ?= no  # Enable keyboard backlight functionality
 AUDIO_ENABLE ?= no
 RGBLIGHT_ENABLE ?= yes
+WPM_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
